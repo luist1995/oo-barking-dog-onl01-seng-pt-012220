41b76e545073cd3764f242c581384cc227d4f231
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-05 03:40:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Your code goes here!
+class Dog 
+  
+  def name=(dog_name) 
+    @this_dogs_name = dog_name 
+  end
+  
+  def name
+    @this_dogs_name
+  end
+  
+  fido.name = Dog.new 
+  
+  
+  
+    
+  def bark
+    puts "woof!"
+  end
+
+end  
+
+
+    
